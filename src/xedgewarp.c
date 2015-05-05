371fd15fa3e58cda2f575d443d5c9cd06551821f
Ingo Bürk <ingo.buerk@tngtech.com>
2015-05-05 16:37:05 +0200

    Initialize X11 connection and root screen.

--- src/xedgewarp.c
+++ src/xedgewarp.c
@@ -2,10 +2,29 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <err.h>
+#include <xcb/xcb.h>
+#include <xcb/xcb_aux.h>
 #include "randr.h"
 #include "types.h"
 #include "globals.h"
 
+xcb_connection_t *connection;
+xcb_window_t root;
+
+static void initialize(void) {
+    int display;
+    connection = xcb_connect(NULL, &display);
+    if (xcb_connection_has_error(connection)) {
+        errx(EXIT_FAILURE, "could not connect to the X server, bailing out.");
+    }
+
+    xcb_screen_t *screen = xcb_aux_get_screen(connection, display);
+    root = screen->root;
+}
+
 int main(void) {
-    // TODO
+    initialize();
+    xcb_disconnect(connection);
+    exit(EXIT_SUCCESS);
 }
