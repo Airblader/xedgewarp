cb99e2a58749a82783620d3bb4c49fe1829975e7
Ingo Bürk <ingo.buerk@tngtech.com>
2015-05-06 12:30:58 +0200

    Add empty stubs for handling CreateNotify and MotionNotify events.

--- include/event.h
+++ include/event.h
@@ -1,7 +1,22 @@
 // vim:ts=4:sw=4:expandtab
 #pragma once
 
+#include <xcb/xcb.h>
+
 /**
  * Enter the X event loop.
  */
 void event_enter_loop(void);
+
+/**
+ * Handle XCB_CREATE_NOTIFY.
+ * This is used to register our events on the created window.
+ */
+void event_handle_create_notify(xcb_create_notify_event_t *event);
+
+/**
+ * Handle XCB_MOTION_NOTIFY.
+ * This is used to detect when the mouse reaches the edge of an
+ * output.
+ */
+void event_handle_motion_notify(xcb_motion_notify_event_t *event);
--- src/event.c
+++ src/event.c
@@ -11,8 +11,34 @@
 void event_enter_loop(void) {
     xcb_generic_event_t *event;
     while ((event = xcb_wait_for_event(connection))) {
-        // TODO handle event
+        switch (event->response_type & ~0x80) {
+            case XCB_CREATE_NOTIFY:
+                event_handle_create_notify((xcb_create_notify_event_t *) event);
+                break;
+            case XCB_MOTION_NOTIFY:
+                event_handle_motion_notify((xcb_motion_notify_event_t *) event);
+                break;
+            default:
+                break;
+        }
 
         FREE(event);
     }
+}
+
+/*
+ * Handle XCB_CREATE_NOTIFY.
+ * This is used to register our events on the created window.
+ */
+void event_handle_create_notify(xcb_create_notify_event_t *event) {
+    // TODO
+}
+
+/*
+ * Handle XCB_MOTION_NOTIFY.
+ * This is used to detect when the mouse reaches the edge of an
+ * output.
+ */
+void event_handle_motion_notify(xcb_motion_notify_event_t *event) {
+    // TODO
 }
