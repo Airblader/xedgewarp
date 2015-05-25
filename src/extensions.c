1fc4fc5bf7d9861f216c6bc772c3b0ce7cb77ee7
Ingo Bürk <ingo.buerk@tngtech.com>
2015-05-25 22:39:40 +0200

    Initialize Xinput extension

--- src/extensions.c
+++ src/extensions.c
@@ -7,9 +7,37 @@ int randr_ext_offset = -1;
 /* The offset of the XInput extension. */
 int xinput_ext_offset = -1;
 
+/* Forward declarations */
+static void extensions_init_randr(void);
+static void extensions_init_xinput(void);
+
 /*
  * Initialize the required X11 extensions.
  */
 void extensions_init(void) {
-    // TODO
+    extensions_init_randr();
+    extensions_init_xinput();
+}
+
+static void extensions_init_randr(void) {
+    const xcb_query_extension_reply_t *reply = xcb_get_extension_data(connection, &xcb_randr_id);
+    if (!reply->present)
+        bail("Your X server does not support the RandR extension, bailing out.");
+
+    randr_ext_offset = reply->first_event;
+
+    xcb_randr_select_input(connection, root,
+        XCB_RANDR_NOTIFY_MASK_SCREEN_CHANGE |
+        XCB_RANDR_NOTIFY_MASK_OUTPUT_CHANGE |
+        XCB_RANDR_NOTIFY_MASK_CRTC_CHANGE |
+        XCB_RANDR_NOTIFY_MASK_OUTPUT_PROPERTY);
+    xcb_flush(connection);
+}
+
+static void extensions_init_xinput(void) {
+    const xcb_query_extension_reply_t *reply = xcb_get_extension_data(connection, &xcb_input_id);
+    if (!reply->present)
+        bail("Your X server does not support the XInput extension, bailing out.");
+
+    xinput_ext_offset = reply->first_event;
 }
